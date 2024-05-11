7b217a96207d53fd9f9c894f90ab8228e4c92283
Dustrunner369 <matthewswift369@gmail.com>
2024-05-11 11:49:02 -0700

    Merge branch 'develop' of https://github.com/Dustrunner369/NETPractice into develop

---
+++ StarterProject/Book/Mapper/BookMapper.cs
@@ -0,0 +1,20 @@
+namespace StarterProject.Mapper;
+
+public class BookMapper
+{
+
+
+    public static Book MapToBook(dynamic bookData)
+    {
+        return new Book()
+        {
+            Title = bookData.title,
+            Price = bookData.price,
+            Author = bookData.author,
+            Description = bookData.description,
+            Rank = bookData.rank,
+            AmazonLink = bookData.amazon_product_url,
+            ImageLink = bookData.book_image
+        };
+    }
+}
